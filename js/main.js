ff3939c752648266089ab60ef12de5a2f64ba1be
evgeniy-raikhel <ewgenrayhel@yandex.ru>
2022-01-19 10:01:45 +0300

    Create: index.html & main.js

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// var number1 = 8;
+// console.log(number1);
+// var summary = number1 + 100;
+// console.log(summary);
+
+// let firstName = "Евгений",
+//     secondName = "Райхель";
+// let result = firstName + " " + secondName;
+// console.log(result);  
+// let fullName = `${secondName} ${firstName}`;
+// console.log(fullName);
+// let maxNumb = Number.MAX_SAFE_INTEGER;
+// console.log(maxNumb);
+// let bigIntNum = 9007199254740991897n
+// console.log(bigIntNum);
+// console.log(typeof bigIntNum);
+// let num1 = 1/0;
+// console.log(num1);
+// let name1 = "Евгений";
+// let result2 = name1 * 1;
+// console.log(result2);
+
+// let empty = null;
+// console.log(empty);
+
+// let num20;
+// console.log(num20); 
+
+// оператор IF
+
+if (10 > 9) {
+   console.log("Hello World");
+}
+
+/*
+
+*/
